90680a1d702568be5dce973ba32263e99d931472
haruna.nishi <haruna.nishi@rakuten.com>
2018-06-14 03:42:19 +0000

    have

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -13,6 +13,7 @@ class UsersController extends Controller
     {
         $user = User::find($id);
         $count_want = $user->want_items()->count();
+        $count_have = $user->have_items()->count();
         $items = \DB::table('items')->join('item_user', 'items.id', '=', 'item_user.item_id')->select('items.*')->where('item_user.user_id', $user->id)->distinct()->paginate(20);
         
 
@@ -21,7 +22,8 @@ class UsersController extends Controller
             'items' => $items,
             
             'count_want' => $count_want,
-            // 'count_have' => $count_have,
+            'count_have' => $count_have,
+            
         ]);
     }
 }
