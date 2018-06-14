be7b37c76e497511c99105e61f58090b6349ce77
haruna.nishi <haruna.nishi@rakuten.com>
2018-06-14 03:02:33 +0000

    want

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -3,13 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
-use App\Http\Controllers\Controller;
-
 use App\Item;
 
-
-class WelcomeController extends Controller
+class ItemsController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -18,10 +14,7 @@ class WelcomeController extends Controller
      */
     public function index()
     {
-         $items = Item::orderBy('updated_at', 'desc')->paginate(20);
-        return view('welcome', [
-            'items' => $items,
-        ]);
+        //
     }
 
     /**
@@ -31,7 +24,34 @@ class WelcomeController extends Controller
      */
     public function create()
     {
-        //
+          $keyword = request()->keyword;
+        $items = [];
+        if ($keyword) {
+            $client = new \RakutenRws_Client();
+            $client->setApplicationId(env('RAKUTEN_APPLICATION_ID'));
+
+            $rws_response = $client->execute('IchibaItemSearch', [
+                'keyword' => $keyword,
+                'imageFlag' => 1,
+                'hits' => 20,
+            ]);
+
+            // Creating "Item" instance to make it easy to handle.（not saving）
+            foreach ($rws_response->getData()['Items'] as $rws_item) {
+                $item = new Item();
+                $item->code = $rws_item['Item']['itemCode'];
+                $item->name = $rws_item['Item']['itemName'];
+                $item->url = $rws_item['Item']['itemUrl'];
+                $item->image_url = str_replace('?_ex=128x128', '', $rws_item['Item']['mediumImageUrls'][0]['imageUrl']);
+                $items[] = $item;
+            }
+        }
+
+        return view('items.create', [
+            'keyword' => $keyword,
+            'items' => $items,
+        ]);
+
     }
 
     /**
@@ -53,7 +73,13 @@ class WelcomeController extends Controller
      */
     public function show($id)
     {
-        //
+        $item = Item::find($id);
+      $want_users = $item->want_users;
+
+      return view('items.show', [
+          'item' => $item,
+          'want_users' => $want_users,
+      ]);
     }
 
     /**
